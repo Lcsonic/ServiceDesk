b3352e5c9d1708cc6ca6589696d4e7dcbfd8547e
lcsonic <lucasaraujoss95@gmail.com>
2019-04-02 21:19:09 -0300

    commit data 02-abr-12:19

--- src/app/service-desk/service-desk-routing.module.ts
+++ src/app/service-desk/service-desk-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
+import { AdicionarComponent } from './adicionar/adicionar.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent},
-  { path: 'menu', component:MenuComponent}
+  { path: 'menu', component:MenuComponent},
+  { path: 'menu/adicionar', component:AdicionarComponent}
   
 ];
 
--- src/app/service-desk/service-desk.module.ts
+++ src/app/service-desk/service-desk.module.ts
@@ -5,11 +5,13 @@ import { ServiceDeskRoutingModule } from './service-desk-routing.module';
 import { GeralModule } from '../geral/geral.module';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
+import { AdicionarComponent } from './adicionar/adicionar.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
     MenuComponent,
+    AdicionarComponent,
   ],
   imports: [
     ServiceDeskRoutingModule,
